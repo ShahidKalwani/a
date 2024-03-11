8f6b35eb642208b80a650a458b9965155ef6f7cc
Shahid Nawaz <80408404+ShahidKalwani@users.noreply.github.com>
2024-03-11 17:01:08 +0500

    Create flt.js

---
+++ flt.js
@@ -0,0 +1,20 @@
+var script = document.createElement('script');
+
+// Set the source attribute to the URL of the script
+script.src = 'https://cdn.jsdelivr.net/npm/requirejs@2.3.6/require.min.js';
+
+// Set the async attribute to true
+script.async = true;
+
+// Append the script element to the document's head or body
+document.head.appendChild(script)
+var script = document.createElement('script');
+
+// Set the source attribute to the URL of the script
+script.src = 'https://cdn.jsdelivr.net/npm/browserify@17.0.0/index.min.js';
+
+// Set the async attribute to true
+script.async = true;
+
+// Append the script element to the document's head or body
+document.head.appendChild(script)
