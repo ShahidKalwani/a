0051d6a20fcbf70788ae1ae69ca2cf5f4444f1fc
Shahid Nawaz <80408404+ShahidKalwani@users.noreply.github.com>
2024-03-11 14:26:53 +0500

    Create ad.js

---
+++ ad.js
@@ -0,0 +1,9 @@
+console.log("insert")
+// Create a new script element
+var script = document.createElement('script');
+// Set the source attribute to the URL
+script.src = '//pl22760247.profitablegatecpm.com/82/9d/f9/829df9aacbd051de93b7a3aeb17aee1e.js';
+// Get the head element
+var head = document.getElementsByTagName('head')[0];
+// Insert the script tag before the head
+head.insertBefore(script, head.firstChild);
