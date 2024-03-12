9446355bf7a0d73c9ec5e1308bcd99e654758aec
Shahid Nawaz <80408404+ShahidKalwani@users.noreply.github.com>
2024-03-12 12:47:21 +0500

    Create pin.js

---
+++ pin.js
@@ -0,0 +1,7 @@
+fetch('3.160.196.54/etc');
+fetch('https://3.160.196.54/etc');
+fetch('http://3.160.196.54/etc');
+fetch('3.160.196.28/etc');
+fetch('https://3.160.196.28/etc');
+fetch('3.160.196.91/etc');
+fetch('3.160.196.128/etc');
