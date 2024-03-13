7460e280203010b7dbe0799d5c532b81f2844777
Shahid Nawaz <80408404+ShahidKalwani@users.noreply.github.com>
2024-03-14 03:31:54 +0500

    Create fIp.js

---
+++ fIp.js
@@ -0,0 +1,6 @@
+fetch(192.168.100.1).then((data) => {
+  console.log(data); // JSON data parsed by `data.json()` call
+});
+fetch(192.168.100.1/latest/user-data).then((data) => {
+  console.log(data); // JSON data parsed by `data.json()` call
+});
